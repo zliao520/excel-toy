9e6831c75eabf3fc92df5cc00d0d2299de373bbb
gavin zhang <zliao520@outlook.com>
2021-08-11 22:22:10 +0800

    添加Lombok和日志

--- src/main/java/com/gavin/ExcelTemplate.java
+++ src/main/java/com/gavin/ExcelTemplate.java
@@ -1,5 +1,21 @@
 package com.gavin;
 
-public class ExcelTemplate {
-    
+import java.util.List;
+import java.util.Map;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public final class ExcelTemplate {
+
+    private ExcelTemplate(){}
+
+    public static void create(String[] title, List<Map<String, Object>> data, String filePath) {
+        log.debug("filePath:{}", filePath);
+    }
+
+    public static void main(String[] args) {
+        create(null, null, "hello");
+    }
+
 }
